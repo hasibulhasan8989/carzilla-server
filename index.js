92c6b73029356fe780f4eb46cc818b2c095b4cd9
Hasibul Hasan <hasanchaion847@gmail.com>
2025-08-05 22:39:54 +0600

    order and test drive

--- index.js
+++ index.js
@@ -30,19 +30,23 @@ async function run() {
       .db("CarZilla")
       .collection("productCollection");
     const cartCollection = client.db("CarZilla").collection("cartCollection");
-
+    const orderCollection = client.db("CarZilla").collection("orderCollection");
+    const testDriveRequest = client
+      .db("CarZilla")
+      .collection("testDriveRequest");
+    // ------------All Products-----------------
     app.get("/products", async (req, res) => {
       const result = await productCollection.find().toArray();
       res.send(result);
     });
-
+    //-------------SingleProduct------------------
     app.get("/products/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await productCollection.findOne(query);
       res.send(result);
     });
-
+    //-------------Post a Cart Item --------------
     app.post("/cart", async (req, res) => {
       const cartItem = req.body;
 
@@ -68,7 +72,7 @@ async function run() {
 
       res.send(result);
     });
-
+    //------------Get Cart Items By Email------------
     app.get("/cart/:email", async (req, res) => {
       const email = req.params.email;
       const query = { email: email };
@@ -77,6 +81,61 @@ async function run() {
       const query2 = { _id: { $in: cartIds } };
       const result = await productCollection.find(query2).toArray();
 
+      res.send(result);
+    });
+
+    //--------------Delete A Cart Item By Id---------
+
+    app.delete("/cart/:id", async (req, res) => {
+      const id = req.params.id;
+      const email = req.query.email;
+      console.log(id);
+      const query = { item: id, email: email };
+      const result = await cartCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    // ----post a order------------
+
+    app.post("/order", async (req, res) => {
+      const order = req.body;
+      const { orderItem, buyerInfo } = order;
+      const email = buyerInfo.email;
+      const itemIds = orderItem.map((card) => {
+        return card.itemId;
+      });
+
+      if (!email || !orderItem.length) {
+        return res.status(400).send({ error: "Invalid order data" });
+      }
+
+      const deleteQuery = {
+        email: email,
+        item: {
+          $in: itemIds,
+        },
+      };
+
+      await cartCollection.deleteMany(deleteQuery);
+      const result = await orderCollection.insertOne(order);
+      res.send(result);
+    });
+
+    //----------------Drive Booking-----------
+
+    app.post("/testDrive", async (req, res) => {
+      const driveRequest = req.body;
+      const query = {
+        email: driveRequest.email,
+        productId: driveRequest.productId,
+      };
+
+      const isExit = await testDriveRequest.findOne(query);
+      if (isExit) {
+        return res.send({ isExit: true });
+      }
+
+      const result = await testDriveRequest.insertOne(driveRequest);
       res.send(result);
     });
   } finally {
